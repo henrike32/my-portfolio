84cc81c2f3a89efbc6d120fa5e38d0dba42efa8f
henrike32 <henrique.lambertucci@gmail.com>
2023-08-10 18:23:02 -0400

    adding new favicon, changing index.html message, erasing some unused code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -23,26 +23,6 @@ export const Contact = () => {
     });
   };
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   setButtonText("Sending...");
-  //   let response = await fetch("http://localhost:5000/contact", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json;charset=utf-8",
-  //     },
-  //     body: JSON.stringify(formDetails),
-  //   });
-  //   setButtonText("Send");
-  //   let result = await response.json();
-  //   setFormDetails(formInitialDetails);
-  //   if (result.code == 200) {
-  //     setStatus({ succes: true, message: 'Message sent successfully'});
-  //   } else {
-  //     setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
-  //   }
-  // };
-
   return (
     <section className="contact" id="connect">
       <Container>
--- src/ideas.md
+++
@@ -1,4 +0,0 @@
-
-Adicionar email
-
-<a href="mailto:youremail@here.com">Want to see more? Send me an email</a>
