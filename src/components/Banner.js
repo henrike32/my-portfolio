950427fa50e8f53b9fbc1a6d3aff8736d0f57404
henrike32 <henrique.lambertucci@gmail.com>
2023-08-15 12:07:38 -0400

    removed animation on picture, it was making the mobile version bad

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -65,7 +65,7 @@ export const Banner = () => {
             <Col xs={12} md={6} xl={5}>
               <TrackVisibility>
                 {({ isVisible }) =>
-                  <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <div className={isVisible}>
                     <img className="profile-pic" src={headerImg} alt="Header Img"/>
                   </div>}
               </TrackVisibility>
